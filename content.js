97afdaffd7cdc89c64e163f9802470b2b5277bd1
safagenctorun <safa_genctorun@hotmail.com>
2025-08-13 10:39:05 +0300

    refactor: update fullscreen handling and remove unnecessary script execution

--- content.js
+++ content.js
@@ -1,15 +1,22 @@
 chrome.storage.sync.get(["enabled"], (result) => {
   if (result.enabled ?? true) {
-    console.log("Extension is enabled. Running content script...");
-    runScript();
+    console.log("Extension is enabled. Listening for fullscreen changes...");
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
   } else {
-    console.log("Extension is disabled. Doing nothing.");
+    console.log("Extension is disabled.");
   }
 });
 
-const titleElement = document.querySelector(".ytp-title-text");
-const gradientElement = document.querySelector(".ytp-gradient-top");
-function runScript() {
+function handleFullscreenChange() {
+  if (document.fullscreenElement) {
+    removeFullscreenElements();
+  }
+}
+
+function removeFullscreenElements() {
+  const titleElement = document.querySelector(".ytp-title-text");
+  const gradientElement = document.querySelector(".ytp-gradient-top");
+
   if (titleElement) {
     titleElement.remove();
   }
